0a3d9a3d294ac2256a1cd5350c03dabc98c183bb
Garry Laly <garrylaly@gmail.com>
2022-03-18 10:48:42 +0700

    Added icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,19 @@
 import IconBack from './ic_back.png';
+import IconBookmark from './ic_bookmark.png';
 import IconClock from './ic_clock.png';
+import IconDrawer from './ic_drawer.png';
+import IconMenu from './ic_menu.png';
+import IconNotif from './ic_notif.png';
 import IconPlay from './ic_play.png';
 import IconStar from './ic_star.png';
 
-export {IconBack, IconClock, IconPlay, IconStar};
+export {
+  IconBack,
+  IconBookmark,
+  IconClock,
+  IconDrawer,
+  IconMenu,
+  IconNotif,
+  IconPlay,
+  IconStar,
+};
